2c368c47c316cb0b0d2a65015959a8309d26c7b3
yxwdsq <yxwdsq@163.com>
2019-05-25 18:09:44 +0800

    other comming

--- newdata/src/main/java/test/newdata/App.java
+++ newdata/src/main/java/test/newdata/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! I'm coming" );
+        System.out.println( "Hello World!" );
+        System.out.println( "I'm comming!" );
     }
 }
